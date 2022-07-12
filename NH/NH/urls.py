fa3c5975e10f485449ce5ec39a0c479415867cad
ZM <2649743806@qq.com>
2022-07-12 10:37:28 +0800

    农户建档信息查询与可视化路由

--- NH/NH/urls.py
+++ NH/NH/urls.py
@@ -16,10 +16,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from role import views
+from role import views, show_documentation_area
+from role.view import search_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',views.hello),
-    path('',TemplateView.as_view(template_name="index.html"),name="home"),
+    path('', TemplateView.as_view(template_name="index.html"), name="home"),
+    # path('search/detail', TemplateView.as_view(template_name="nh_detail.html"), name="detail"),
+    path('search/detail', search_view.trandi, name="detail"),
+    # path('search/', search_view.test, name="search"),
+    path('search/', search_view.SearchView.as_view(), name="search"),
+    # path('test/', search_view.SearchView.as_view(), name="test"),
+    # path('search/<int:page_index>', search_view.SearchView.as_view()),
+    # path('area_show/', TemplateView.as_view(template_name="area_show.html"), name="area_show"),
+    path('content/', TemplateView.as_view(template_name="content.html"), name="content"),
+
+    path('area_show1/', show_documentation_area.show_area1, name="show1"),
+    path('area_show2/', show_documentation_area.show_area2, name="show2"),
+    path('area_show3/', show_documentation_area.show_area3, name="show3"),
+    path('area_show4/', show_documentation_area.show_area4, name="show4"),
+    path('area_show5/', show_documentation_area.run_show, name="show5"),
+    path('area_show6/', show_documentation_area.show_area6, name="show6"),
+
+    path('show/', TemplateView.as_view(template_name="area_show.html"), name="area_show"),
+
 ]
